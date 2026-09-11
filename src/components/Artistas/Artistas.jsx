068a47e3db9f9ea57b1fd1690ea219c8872b9f66
Brian Quintanilla <quintanillaxuya2b38@gmail.com>
2026-09-10 19:37:40 -0600

    page artistas fin

--- src/components/Artistas/Artistas.jsx
+++ src/components/Artistas/Artistas.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './Artistas.css'
 import Artista1 from '../../assets/artista1.avif';
+import Artista2 from '../../assets/artista2.webp';
+import Artista3 from '../../assets/artista3.webp';
+import Artista4 from '../../assets/artista4.webp';
+import Artista5 from '../../assets/artista5.jpg';
+import Artista6 from '../../assets/artista6.jpg';
 
 const Artistas = () => {
     return (
@@ -17,31 +22,31 @@ const Artistas = () => {
                 </div>
 
                 <div className="tarjeta-artista">
-                    <img src="" alt="Artista 2" />
+                    <img src={Artista2} alt="Artista 2" />
                     <h2>Artista 2</h2>
                     <p>Especialista en retratos.</p>
                 </div>
 
                 <div className="tarjeta-artista">
-                    <img src="" alt="Artista 3" />
+                    <img src={Artista3} alt="Artista 3" />
                     <h2>Artista 3</h2>
                     <p>Especialista en realismo.</p>
                 </div>
 
                 <div className="tarjeta-artista">
-                    <img src="" alt="Artista 4" />
+                    <img src={Artista4} alt="Artista 4" />
                     <h2>Artista 4</h2>
                     <p>Especialista en tatuajes tradicionales.</p>
                 </div>
 
                 <div className="tarjeta-artista">
-                    <img src="" alt="Artista 5" />
+                    <img src={Artista5} alt="Artista 5" />
                     <h2>Artista 5</h2>
                     <p>Especialista en diseños personalizados.</p>
                 </div>
 
                 <div className="tarjeta-artista">
-                    <img src="" alt="Artista 6" />
+                    <img src={Artista6} alt="Artista 6" />
                     <h2>Artista 6</h2>
                     <p>Especialista en diferentes estilos.</p>
                 </div>
